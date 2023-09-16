8671db739904b715d176456c74b02c93f5fc4444
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-16 22:27:00 +0300

    Add files via upload

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
+/**
+ * _printf - function that produces output according to a format
+ * @format: a pointer to const char holds a string
+ *
+ * Description: a program like printf
+ *
+ * Return: the value of count
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	int count = 0, i;
+	char c, *s;
+
+	va_start(arg, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					c = (char)va_arg(arg, int);
+					putchar(c);
+					count++;
+					break;
+				case 's':
+					s = va_arg(arg, char *);
+					while (*s) 
+					{
+						putchar(*s++);
+						count++;
+					}
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					puts("Invalid format specifier");
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			count++;
+		}
+	}
+	va_end(arg);
+
+	return count;
+}
