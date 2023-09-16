f45ada9a33e7e5fb751f0276725d673ce871f5fe
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-17 00:30:20 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdarg.h>
 
+int _define_specifier(char x, va_list arg);
+
 /**
  * _printf - function that produces output according to a format
  * @format: a pointer to const char holds a string
@@ -12,40 +14,23 @@
  * Return: the value of count
  */
 
+
 int _printf(const char *format, ...)
 {
 	va_list arg;
 	int count = 0, i;
-	char c, *s;
 
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	va_start(arg, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			switch (format[i])
-			{
-				case 'c':
-					c = (char)va_arg(arg, int);
-					putchar(c);
-					count++;
-					break;
-				case 's':
-					s = va_arg(arg, char *);
-					while (*s) 
-					{
-						putchar(*s++);
-						count++;
-					}
-					break;
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				default:
-					puts("Invalid format specifier");
-			}
+			count += _define_specifier(format[i], arg);
 		}
 		else
 		{
@@ -54,6 +39,43 @@ int _printf(const char *format, ...)
 		}
 	}
 	va_end(arg);
+	return (count);
+}
 
-	return count;
+/**
+ * _define_specifier - a function defines the type of specifier
+ * @x: a character
+ * @arg: a variadic list of arguments
+ * Return: the value of count
+ */
+
+int _define_specifier(char x, va_list arg)
+{
+	char c, *s;
+	int count = 0;
+
+	switch (x)
+	{
+		case 'c':
+			c = (char)va_arg(arg, int);
+			putchar(c);
+			count++;
+			break;
+		case 's':
+			s = va_arg(arg, char *);
+			while (*s)
+			{
+				putchar(*s++);
+				count++;
+			}
+			break;
+		case '%':
+			putchar('%');
+			count++;
+			break;
+		default:
+			return (-1);
+	}
+
+	return (count);
 }
