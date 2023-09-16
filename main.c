dffcdb0a6357f6442801868959f2c4024b6e8d83
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-17 02:01:55 +0300

    Update main.c

--- main.c
+++ main.c
@@ -11,27 +11,5 @@ int main(void)
 {
     _printf("hello % hello /n");
     printf("/n");
-    _printf("hello %");
-    printf("/n");
-    _printf("% hello/n");
-    printf("/n");
-    _printf(NULL);
-    printf("/n");
-    _printf("");
-    printf("/n");
-    _printf("%");
-    printf("/n");
-    _printf("hello %hello");
-    printf("/n");
-    _printf("%z");
-    printf("/n");
-    _printf("% c" ,'a');
-     printf("/n");
-   _printf("hello %hello");
-    printf("/n");
-    _printf("%s");
-     printf("/n");
-   _printf("hello","world");
-        printf("/n");
     return (0);
 }
