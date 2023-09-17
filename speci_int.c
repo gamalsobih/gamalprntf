86626ccb95365cb76bc31a73e60490b5fd4caf45
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 02:49:40 +0300

    Update speci_int.c

--- speci_int.c
+++ speci_int.c
@@ -10,7 +10,7 @@ char *speci_int(va_list arg)
 {
 	int in = va_arg(arg, int);
 	char buffer [sizeof(int)*8+1];
-	 itoa (in,buffer,DECIMAL);
+	 itoa (in,buffer,10);
 	if (buffer == NULL)
 		return (NULL);
 
