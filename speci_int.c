e21b47534d74f3357e7a74628666f799af1f2b9e
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 03:58:00 +0300

    Update speci_int.c

--- speci_int.c
+++ speci_int.c
@@ -11,7 +11,7 @@ char *speci_int(va_list arg)
 	char *buffer;
 
 	buffer = malloc(sizeof(int) * 8 + 1);
-	 itoa (va_arg(arg, int), buffer, 10);
+	 itoa(va_arg(arg, int), buffer, 10);
 	if (buffer == NULL)
 		return (NULL);
 
