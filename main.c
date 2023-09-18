16a1156fc46f4aaca1e8955e1cfdf4cf5e7ab151
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 05:18:44 +0300

    Update main.c

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(void)
     addr = (void *)0x7ffe637541f0;
     _printf("Length:[%i]\n",len);
     printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%b]\n", 56);
+    _printf("Negative:[%b]\n", -98);
     printf("Negative:[%d]\n", -762534);
     _printf("Unsigned:[%u]\n", ui);
     printf("Unsigned:[%u]\n", ui);
