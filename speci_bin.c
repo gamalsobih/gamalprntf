f2dc36ade4e5d624b0e5d0280cd36fbb0d2cf6c3
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 05:34:07 +0300

    Update speci_bin.c

--- speci_bin.c
+++ speci_bin.c
@@ -8,7 +8,7 @@
 int int_to_binary(unsigned int k)
 {
 if (k == 0 || k == 1)
-		    return (k);
+	return (k);
 return ((k % 2) + 10 * int_to_binary(k / 2));
 }
 
@@ -16,7 +16,7 @@ char *speci_bin(va_list arg)
 {
 	char *buffer;
 	unsigned int bin;
-  int x =va_arg(arg, unsigned int);
+  int x = va_arg(arg, unsigned int);
 	if( x<0)
 		x*=-1;
 	bin =int_to_binary(x);
