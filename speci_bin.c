fbb223940b63fd2606a926b1c7a93ba534d67e85
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 05:21:14 +0300

    Update speci_bin.c

--- speci_bin.c
+++ speci_bin.c
@@ -16,6 +16,8 @@ char *speci_bin(va_list arg)
 {
 	char *buffer;
   unsigned int x =va_arg(arg, int);
+	if(x<0)
+		x*=-1;
   unsigned int bin =int_to_binary(x);
 
 	buffer = malloc(sizeof(int) * 8 + 1);
