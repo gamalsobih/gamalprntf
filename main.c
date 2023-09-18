3553922626994a8433a732f1eb7b7bffecac4b3d
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 05:08:53 +0300

    Update main.c

--- main.c
+++ main.c
@@ -15,6 +15,7 @@ int main(void)
     void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
+    _printf("%b\n", 98);
     len2 = printf("Let's try to printf a simple sentence.\n");
     ui = (unsigned int)INT_MAX + 1024;
     addr = (void *)0x7ffe637541f0;
