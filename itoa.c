5a5f71bc5e985c8cc5b39421cfa05e5a67b41cf2
gamalsobih <133385330+gamalsobih@users.noreply.github.com>
2023-09-18 03:15:32 +0300

    Update itoa.c

--- itoa.c
+++ itoa.c
@@ -1,6 +1,6 @@
 #include "main.h"
 char* itoa(int value, char* str, int base) {
-    int i ;
+    int i ,length;
     char* ptr;
     /* Handle negative numbers*/
     if (value < 0 && base == 10) {
@@ -20,7 +20,7 @@ char* itoa(int value, char* str, int base) {
     *ptr = '\0';
 
     /* Reverse the string*/
-    int length = ptr - str;
+    length = ptr - str;
     for (i = 0; i < length / 2; i++) {
         char temp = str[i];
         str[i] = str[length - i - 1];
